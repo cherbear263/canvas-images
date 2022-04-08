a39b19b2574d5ea3890f6c07f2d0201ca6d09a0b
msjones3 <cheryl@realsocial.com.au>
2022-04-08 13:08:12 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Image manipulation in Canvas
+
+## About the project
+
+Some examples of simple ways to manipulate images using HTML5 canvas and javascript. I'm working my way towards resizing multiple images before submitting to a server
+
+### features
+
+- drawing on html5 canvas, copying to an image tag and downloading
+
+- uploading multiple files
+
+## Built With
+
+- vanilla JS
+- Tailwindss
+
+## Getting Started
+
+Install tailwind
+`npm install`
+
+To watch and reload styles:
+`npx tailwindcss -i ./main.css -o ./dist/output.css --watch`
+
+## Acknowledgements
+
+- Chris Dixon did a fantastic Youtube video on HTML5 upload and download [HTML5 upload and download](https://www.youtube.com/watch?v=sA07GElhqzg)
---
+++ js/previewImages.js
@@ -0,0 +1,27 @@
+      // ********************** upload and resize multiple images ****************/
+      // files will already be in a list. Loop through the list 
+      // the displayed canvas can be limited by the parent div with max-width and not affect the canvas size
+      let fileInput = document.getElementById("file-input");
+      let imageContainer = document.getElementById("images");
+      let numOfFiles = document.getElementById("num-of-files");
+      console.log('started')
+      function preview() {
+        imageContainer.innerHTML = "";
+        numOfFiles.textContent =  `${fileInput.files.length} Files Selected`;
+        for(i of fileInput.files) {
+          let reader = new FileReader();
+          let figure = document.createElement("figure");
+          let figCap = document.createElement("figcaption")
+          ;
+          figCap.innerText = i.name;
+          figure.appendChild(figCap);
+          reader.onload=() => {
+            let img = document.createElement("img");
+            img.setAttribute("src", reader.result);
+            figure.insertBefore(img, figCap);
+          }
+          imageContainer.appendChild(figure);
+          reader.readAsDataURL(i)
+        }
+
+      }
